5f8239714cca832276dc59998451f7c1c09d81ba
João Vitor Madureira <103207658+j-madureira@users.noreply.github.com>
2022-04-20 13:58:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/*Crie um array e utilize FOR OF para fazer uma varredura para retornar os valores
+do Array
+*/
+
+var pessoas = ["Jonathan","João","Rafaele","Tamires","Dierbo","Baguara"];
+
+for(var i of pessoas){
+    console.log(i);
+}
